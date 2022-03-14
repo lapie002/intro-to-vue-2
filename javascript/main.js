eaa41a40d8b3e8b4b48551faf49c7ba9fb1b0983
bruno lapierre <bruno.lapierre@gallea.ca>
2022-03-14 17:47:35 -0400

    v-for example

--- javascript/main.js
+++ javascript/main.js
@@ -4,6 +4,23 @@ let app = new Vue({
         product: 'Socks',
         description: 'A pair of warm, fuzzy socks',
         image: './assets/vmSocks-green-onWhite.jpg',
-        link: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg'
+        link: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
+        inStock: true,
+        onSale: false,
+        details:['80% cotton', '20% polyester', 'Gender-neutral'],
+        variants: [
+            {
+                variantId: 1,
+                variantColor: 'blue'
+            },
+            {
+                variantId: 2,
+                variantColor: 'green'
+            },
+            {
+                variantId: 3,
+                variantColor: 'pink'
+            }
+        ],
     }
 })
