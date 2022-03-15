08e2e713b6b52eff9b92eb01a33a81f0bf0d9a97
bruno lapierre <bruno.lapierre@gallea.ca>
2022-03-15 11:42:48 -0400

    Computed Properties

--- javascript/main.js
+++ javascript/main.js
@@ -2,27 +2,48 @@ let app = new Vue({
     el: '#app',
 
     data: {
+        brand: 'Vue Mastery',
         product: 'Socks',
+        selectedVariant: 0,
         description: 'A pair of warm, fuzzy socks',
-        image: './assets/vmSocks-green-onWhite.jpg',
         link: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
-        inStock: true,
-        onSale: false,
         details:['80% cotton', '20% polyester', 'Gender-neutral'],
         variants: [
             {
                 variantId: 2234,
                 variantColor: '#4cb686',
-                variantImage: './assets/vmSocks-green-onWhite.jpg'
+                variantImage: './assets/vmSocks-green-onWhite.jpg',
+                variantQuantity: 10,
+                variantOnSale: true
             },
             {
                 variantId: 2235,
                 variantColor: '#445a72',
-                variantImage: './assets/vmSocks-blue-onWhite.jpg'
+                variantImage: './assets/vmSocks-blue-onWhite.jpg',
+                variantQuantity: 0,
+                variantOnSale: false
             }
         ],
         sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
         cart: 0
+    },
+
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product
+        },
+
+        image() {
+            return this.variants[this.selectedVariant].variantImage
+        },
+
+        inStock() {
+            return this.variants[this.selectedVariant].variantQuantity
+        },
+
+        onSale() {
+            return !!this.variants[this.selectedVariant].variantQuantity
+        }
     },
 
     methods: {
@@ -37,8 +58,8 @@ let app = new Vue({
             }
         },
 
-        updateProduct(image) {
-            this.image = image
+        updateProduct(index) {
+            this.selectedVariant = index
         }
     }
 })
