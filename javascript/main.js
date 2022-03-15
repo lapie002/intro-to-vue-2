bab90a28a3c1d88902723d3a8d322b906ab88ad3
bruno lapierre <bruno.lapierre@gallea.ca>
2022-03-15 16:25:18 -0400

    split project into Components

--- javascript/main.js
+++ javascript/main.js
@@ -1,65 +1,106 @@
-let app = new Vue({
-    el: '#app',
-
-    data: {
-        brand: 'Vue Mastery',
-        product: 'Socks',
-        selectedVariant: 0,
-        description: 'A pair of warm, fuzzy socks',
-        link: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
-        details:['80% cotton', '20% polyester', 'Gender-neutral'],
-        variants: [
-            {
-                variantId: 2234,
-                variantColor: '#4cb686',
-                variantImage: './assets/vmSocks-green-onWhite.jpg',
-                variantQuantity: 10,
-                variantOnSale: true
-            },
-            {
-                variantId: 2235,
-                variantColor: '#445a72',
-                variantImage: './assets/vmSocks-blue-onWhite.jpg',
-                variantQuantity: 0,
-                variantOnSale: false
-            }
-        ],
-        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
-        cart: 0
+Vue.component('product', {
+    props: {
+        premium: {
+            type: Boolean,
+            required: true
+        }
     },
 
+    template: `
+   <div class="product">
+        
+      <div class="product-image">
+        <img :src="image" />
+      </div>
+
+      <div class="product-info">
+          <h1>{{ title }}</h1>
+          <p v-if="inStock">In Stock</p>
+          <p v-else>Out of Stock</p>
+          <p>Shipping: {{ shipping }}</p>
+
+          <ul>
+            <li v-for="detail in details">{{ detail }}</li>
+          </ul>
+
+          <div class="color-box"
+               v-for="(variant, index) in variants" 
+               :key="variant.variantId"
+               :style="{ backgroundColor: variant.variantColor }"
+               @mouseover="updateProduct(index)"
+               >
+          </div> 
+
+          <button v-on:click="addToCart" 
+            :disabled="!inStock"
+            :class="{ disabledButton: !inStock }"
+            >
+          Add to cart
+          </button>
+
+          <div class="cart">
+            <p>Cart({{ cart }})</p>
+          </div>
+
+       </div>  
+    
+    </div>
+   `,
+
+    data() {
+        return {
+            product: 'Socks',
+            brand: 'Vue Mastery',
+            selectedVariant: 0,
+            details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+            variants: [
+                {
+                    variantId: 2234,
+                    variantColor: '#4cb686',
+                    variantImage:  'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
+                    variantQuantity: 10
+                },
+                {
+                    variantId: 2235,
+                    variantColor: '#445a72',
+                    variantImage: 'https://www.vuemastery.com/images/challenges/vmSocks-blue-onWhite.jpg',
+                    variantQuantity: 0
+                }
+            ],
+            cart: 0
+        }
+    },
+    methods: {
+        addToCart: function() {
+            this.cart += 1
+        },
+        updateProduct: function(index) {
+            this.selectedVariant = index
+        }
+    },
     computed: {
+
         title() {
             return this.brand + ' ' + this.product
         },
-
-        image() {
+        image(){
             return this.variants[this.selectedVariant].variantImage
         },
-
-        inStock() {
+        inStock(){
             return this.variants[this.selectedVariant].variantQuantity
         },
-
-        onSale() {
-            return !!this.variants[this.selectedVariant].variantQuantity
-        }
-    },
-
-    methods: {
-
-        addToCart() {
-            this.cart += 1
-        },
-
-        removeFromCart() {
-            if (this.cart !== 0) {
-                this.cart -= 1
+        shipping() {
+            if (this.premium) {
+                return "Free"
             }
-        },
-
-        updateProduct(index) {
-            this.selectedVariant = index
+            return 2.99
         }
+    }
+})
+
+let app = new Vue({
+    el: '#app',
+    data: {
+        premium: true
     }
 })
